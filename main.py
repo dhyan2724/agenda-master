23099732e5a0cc9ac80eaef9eaa070e0ed26f253
dhyan2724 <127980478+dhyan2724@users.noreply.github.com>
2023-11-01 12:04:45 +0530

    uygyu

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 from flask import Flask, render_template
+import os
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
-app = Flask(__name__)
+app = Flask(__name__	)
 
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
 # the associated function.
+
+print(os.listdir())
+
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
